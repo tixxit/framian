eecded74cf598adae8fe5183bdcb10cfed5f6e22
Charles Francis <charles.francis@pellucid.com>
2013-11-26 18:16:03 -0500

    Trying to fix a strange issue with categorical scale...
    Csv parser compiles, need to test.

--- src/main/scala/pellucid/pframe/FrameUtils.scala
+++ src/main/scala/pellucid/pframe/FrameUtils.scala
@@ -8,6 +8,7 @@ import scala.collection.mutable.ArrayBuffer
 
 import scala.io.Source
 import spire.math.Number
+import spire.implicits._
 
 object Utilities {
 
@@ -20,18 +21,18 @@ object Utilities {
   def csv(
     location: String,
     delimiter: String = ",",
-    quote: String = '"',
+    quote: String = "\"",
     rowIndex: Option[Int] = None,
     columnIndex: Boolean = false,
     contentColumnsToParse: List[Int] = List()
   ) = {
+    // sometimes you just want the bulk of the state-machine compiled for you...
     val stripWhitespaceCheckQuoteState =
       s"(?:[\\s]*($quote[^$quote]*$quote|[^\\s$quote]*)[\\s]*)|[\\s]*($quote[^$quote]*)".r
     val checkQuoteFinished = s"([^$quote]*)|([^$quote]*$quote)[\\s]*".r
-    val colsPlusIndex = rowIndex.fold(columnsToParse)(_ :: columnsToParse).sorted
 
     // assuming sorted list of desired values...
-    def parseLine(line: String, columns: List[Int] = List()) = {
+    def parseLine(line: String, columns: List[Int] = List()): ArrayBuffer[String] = {
       val lineScanner = new Scanner(line).useDelimiter(delimiter)
       var inQuote = false
       var position = 0
@@ -66,29 +67,43 @@ object Utilities {
 
     val file = new BufferedReader(new FileReader(location))
     var nextLine = file.readLine()
+    var index = 0
 
     // if you want a row index, you don't have to explicitly specify the first column in columnsToParse
     val columnsToParse = rowIndex.fold(contentColumnsToParse) {
       indexPosition =>
-      if (!contentColumnsToParse.isEmpty && !contentColumnsToParse.contains(indexPosition)) indexPosition :: contentColumnsToParse
-      else contentColumnsToParse
+      if (!contentColumnsToParse.isEmpty && !contentColumnsToParse.contains(indexPosition))
+        indexPosition :: contentColumnsToParse
+      else
+        contentColumnsToParse
     }
     // first line might be the column index and not real values, also want to instantiate column cache
     val firstParsedLine = parseLine(nextLine, columnsToParse)
     val numberOfColumns = firstParsedLine.length
-    val columnsSeq = 0 to numberOfColumns
+    val columnsSeq = 0 to numberOfColumns map (_.toString)
     val (colIndexArray, columns) =
-      if (columnIndex) (firstParsedLine, ArrayBuffer[ArrayBuffer[String]](columnsSeq map { _ => ArrayBuffer[String]() }))
-      else (Array(columnsSeq: _*), ArrayBuffer[ArrayBuffer[String]]())
+      if (columnIndex)
+        (firstParsedLine, ArrayBuffer[ArrayBuffer[String]](columnsSeq map { _ => ArrayBuffer[String]() }: _*))
+      else
+        (ArrayBuffer(columnsSeq: _*), ArrayBuffer[ArrayBuffer[String]]())
 
     while (nextLine != null) {
-      val parsed = parseLine(nextLine, colsPlusIndex)
+      val parsed = parseLine(nextLine, columnsToParse)
+      while (index < numberOfColumns) {
+        columns(index) += parsed(index)
+        index += 1
+      }
 
       nextLine = file.readLine()
-      position += 1
+      index = 0
     }
     file.close()
 
-    Frame(Index(columns(0)), colIndexArray.zip(columns(1:columns.length)))
+    Frame(
+      Index(columns(0).toArray),
+      colIndexArray.zip(
+        columns.slice(1, columns.length) map {
+          colArr => TypedColumn(Column.fromArray(colArr.toArray))
+        }): _*)
   }
 }
