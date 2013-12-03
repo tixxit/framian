4600ce3809fe05f6409e5118d4e237e2ff6aaef3
Charles Francis <charles.francis@pellucid.com>
2013-12-02 19:32:30 -0500

    Various helpers for building frame objects needed to efficiently build pframe from current metrics implementation.
    Note: the multi-series join code using hlists "compiles" but is
    incredibly messy and untested at present. Very very wip... Beware!

--- src/main/scala/pellucid/pframe/Frame.scala
+++ src/main/scala/pellucid/pframe/Frame.scala
@@ -5,7 +5,9 @@ import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
 
 import spire.algebra._
+import spire.implicits._
 import shapeless._
+import shapeless.ops.hlist._
 
 trait Frame[Row, Col] {
   def rowIndex: Index[Row]
@@ -26,23 +28,87 @@ trait Frame[Row, Col] {
   def withColIndex[C1](ci: Index[C1]): Frame[Row, C1]
   def withRowIndex[R1](ri: Index[R1]): Frame[R1, Col]
 
-  def join(that: Frame[Row, Col])(join: Join): Frame[Row, Col] = {
+  private def genericJoin[T](
+    getIndex: T => Index[Row],
+    reindexColumns: Array[Int] => T => Seq[(Col, UntypedColumn)]
+  )(that: T)(join: Join): Frame[Row, Col] = {
     // TODO: This should use simpler things, like:
     //   this.reindex(lIndex).withRowIndex(newRowIndex) ++
     //   that.reindex(rIndex).withRowIndex(newRowIndex)
-
     val joiner = Joiner[Row](join)(rowIndex.classTag)
-    val (keys, lIndex, rIndex) = Index.cogroup(this.rowIndex, that.rowIndex)(joiner).result()
+    val (keys, lIndex, rIndex) = Index.cogroup(this.rowIndex, getIndex(that))(joiner).result()
     val newRowIndex = Index.ordered(keys)
     val cols0 = this.getColumns map { case (key, col) =>
       (key, col.setNA(Joiner.Skip).reindex(lIndex))
     }
-    val cols1 = that.getColumns map { case (key, col) =>
-      (key, col.setNA(Joiner.Skip).reindex(rIndex))
-    }
+    val cols1 = reindexColumns(rIndex)(that)
+
     val (newColIndex, cols) = (cols0 ++ cols1).unzip
     ColOrientedFrame(newRowIndex, Index(newColIndex.toArray), cols.toArray)
   }
+
+  def join(that: Frame[Row, Col])(join: Join): Frame[Row, Col] =
+    genericJoin[Frame[Row, Col]](
+      { frame: Frame[Row, Col] => frame.rowIndex },
+      { rIndex: Array[Int] => frame: Frame[Row, Col] =>
+        frame.getColumns map { case (key, col) =>
+          (key, col.setNA(Joiner.Skip).reindex(rIndex))
+        }
+      }
+    )(that)(join)
+
+  //def f[FooList <: HList : <<:[Foo[_]]#λ](foos: FooList)(implicit map: shapeless.ops.hlist.Mapper[fooFunc.type, FooList]) =
+  //         foo map fooFunc
+
+  import LUBConstraint.<<:
+  def join[TSeries <: HList : <<:[Series[Row, _]]#λ]
+          (them: TSeries, columnKeys: Seq[Col] = Seq())
+          (join: Join): Frame[Row, Col] = {
+    import Zipper._
+    import Nat._
+
+    val joiner = Joiner[Row](join)(rowIndex.classTag)
+    object getIndex extends Poly2 {
+      implicit def caseT[T] = at[Index[Row], Series[Row, T]] {
+        (acc, series) => Index.cogroup(acc, series.index)(joiner).result()
+      }
+    }
+
+    //implicit val map: Mapper[getIndex.type, TSeries] = implicitly[Mapper[getIndex.type, TSeries]]
+    implicit lazy val folder: LeftFolder[TSeries, Index[Row], getIndex.type] =
+      implicitly[LeftFolder[TSeries, Index[Row], getIndex.type]]
+
+    genericJoin[TSeries](
+      { series: TSeries => series.foldLeft(Index[Row]())(getIndex).asInstanceOf[Index[Row]] },
+      { rIndex: Array[Int] => series: TSeries =>
+
+        object makeColumns extends Poly2 {
+          val columnIndex =
+            if (columnKeys.isEmpty) 0 to columnKeys.length toArray else columnKeys.toArray
+
+          implicit def caseT[T: TypeTag] = at[(Int, List[(Col, UntypedColumn)]), Series[Row, T]] {
+            case ((i, columns), currentSeries) =>
+              (i + 1,
+               (columnIndex(i),
+                TypedColumn(
+                  currentSeries.column.setNA(Joiner.Skip).reindex(rIndex))
+               ) :: columns)
+          }
+        }
+        implicit lazy val folder: LeftFolder[TSeries, (Int, List[(Col, UntypedColumn)]), makeColumns.type] =
+          implicitly[LeftFolder[TSeries, (Int, List[(Col, UntypedColumn)]), makeColumns.type]]
+
+        series.foldLeft((0, List[(Col, UntypedColumn)]()))(makeColumns).asInstanceOf[(Int, List[(Col, UntypedColumn)])]._2
+      })(them)(join)
+  }
+
+  def join[T: TypeTag](that: Series[Row, T], columnKey: Col)(join: Join): Frame[Row, Col] =
+    genericJoin[Series[Row, T]](
+      { series: Series[Row, T] => series.index },
+      { rIndex: Array[Int] => series: Series[Row, T] =>
+        Seq((columnKey, TypedColumn(series.column.setNA(Joiner.Skip).reindex(rIndex))))
+      })(that)(join)
+
 }
 
 case class ColOrientedFrame[Row, Col](
@@ -78,5 +144,17 @@ object Frame {
   def apply[Row,Col: Order: ClassTag](rowIndex: Index[Row], colPairs: (Col,UntypedColumn)*): Frame[Row,Col] = {
     val (colKeys, cols) = colPairs.unzip
     ColOrientedFrame(rowIndex, Index(colKeys.toArray), cols.toArray)
+  }
+
+  /*def apply[Row,Col: Order: ClassTag](colIndex: Index[Col], colSeries: Series[Row, ]*): Frame[Row,Col] = {
+    val (colKeys, cols) = colPairs.unzip
+    ColOrientedFrame(rowIndex, Index(colKeys.toArray), cols.toArray)
+  }*/
+
+
+
+  def apply[Col: Order: ClassTag](colPairs: (Col,UntypedColumn)*): Frame[Int,Col] = {
+    val (colKeys, cols) = colPairs.unzip
+    ColOrientedFrame(Index(0 to (colKeys.length - 1) toArray), Index(colKeys.toArray), cols.toArray)
   }
 }
