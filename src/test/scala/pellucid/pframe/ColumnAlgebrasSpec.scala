af8c97182eb1cd424ee695f9daaaba0e080ce1b1
Tom Switzer <thomas.switzer@gmail.com>
2013-12-05 11:15:41 -0500

    Use Specs2 to run Column property tests.

--- src/test/scala/pellucid/pframe/ColumnAlgebrasSpec.scala
+++ src/test/scala/pellucid/pframe/ColumnAlgebrasSpec.scala
@@ -1,5 +1,10 @@
 package pellucid.pframe
 
+import org.specs2.mutable._
+import org.specs2.specification.{ Fragments, Fragment, Example} 
+import org.specs2.matcher.Parameters
+import org.specs2.ScalaCheck
+
 import org.scalacheck._
 import org.scalacheck.Arbitrary.arbitrary
 
@@ -9,7 +14,7 @@ import spire.laws._
 import spire.std.int._
 import spire.syntax.eq._
 
-object ColumnAlgebrasSpec extends Properties("ColumnAlgebras") {
+class ColumnAlgebrasSpec extends Specification with ScalaCheck /* extends Properties("ColumnAlgebras")*/ {
   import ColumnGenerators._
 
   // We use a pretty sketchy notion of equality here. Basically, pretending that
@@ -27,12 +32,31 @@ object ColumnAlgebrasSpec extends Properties("ColumnAlgebras") {
     Arbitrary(genSparseColumn(arbitrary[A]))
 
   def genRational: Gen[Rational] = for {
-    n <- arbitrary[Long] filter (_ != Long.MinValue)
-    d <- arbitrary[Long] filter (_ != Long.MinValue) filter (_ != 0)
+    n <- arbitrary[Long] map {
+      case Long.MinValue => Long.MinValue + 1
+      case n => n
+    }
+    d <- arbitrary[Long] map {
+      case 0L => 1L
+      case Long.MinValue => Long.MinValue + 1
+      case n => n
+    }
   } yield Rational(n, d)
 
   implicit def arbRational = Arbitrary(genRational)
 
-  include(RingLaws[Column[Int]].ring)
-  include(RingLaws[Column[Rational]].field)
+  def checkAll(props: Properties): Fragments = {
+    val examples: Seq[Example] = for {
+      (name, prop) <- props.properties
+    } yield {
+      name ! check(prop)(Parameters(maxDiscardRatio = 20F))
+    }
+
+    Fragments.createList(examples: _*)
+  }
+
+  "ColumnAlgebras" should {
+    checkAll(RingLaws[Column[Int]].ring)
+    checkAll(RingLaws[Column[Rational]].field)
+  }
 }
