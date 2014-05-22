0eecae455441ecee80dcaaf41f20dbd8eaeb7648
Daniel James <dwhjames@gmail.com>
2014-05-22 17:10:18 -0400

    expand the Cell trait to make it more like Option
    copy and adapt the methods of Scala’s Option for the Cell trait.

--- modules/pframe/src/main/scala/pellucid/pframe/Cell.scala
+++ modules/pframe/src/main/scala/pellucid/pframe/Cell.scala
@@ -18,43 +18,198 @@ import spire.syntax.semigroup._
   * @see [[Value]] [[[NonValue]]] [[NA]] [[NM]]
   */
 sealed trait Cell[+A] {
+
+  /** Returns true if this [[Cell]] is a value that is available and meaningful.
+    *
+    * @return true if this [[Cell]] is a value that is available and meaningful.
+    * @see [[Value]]
+    */
   def isValue: Boolean
 
+  /** Returns true if this [[Cell]] is not a value that is available and meaningful.
+    *
+    * @return true if this [[Cell]] is not a value that is available and meaningful.
+    * @see [[NonValue]]
+    * @see [[NA]]
+    * @see [[NM]]
+    */
+  @inline final def isNonValue: Boolean = !isValue
+
+  /** Return the [[Cell]]'s value.
+    *
+    * @note The [[Cell]]'s value must be both available and meaningful.
+    * @throws NoSuchElementException if the value is unavailable or not meaningful.
+    */
+  def get: A
+
+  /** Project this [[Cell]] to an `Option`
+    *
+    * @return Some of the value or None for non values
+    */
   def value: Option[A]
+
+  /** Returns the contents of the [[Cell]] as a `String`
+    *
+    * @note {{{
+    * NA.valueString == "NA"
+    * NA.valueString == "NM"
+    * Value(1D).valueString == "1.0"
+    *
+    * NA.toString == "NA"
+    * NM.toString == "NM"
+    * Value(1D).toString == "Value(1.0)"
+    * }}}
+    *
+    * @return the content of the [[Cell]] as a `String`.
+    */
   def valueString: String
 
-  def fold[B](na: => B, nm: => B)(f: A => B): B = this match {
+  /** Returns the result of applying `f` to this [[Cell]]'s value.
+    * Otherwise, evaluates expression `na` if a value is not
+    * available, or evaluates expression `nm` if the value is
+    * not meaningful.
+    *
+    * @param  na  the expression to evaluate if no value is available.
+    * @param  nm  the expression to evaluate if the value in not meaningful.
+    * @param  f   the function to apply to a value that is available and meaningful.
+    */
+  @inline final def fold[B](na: => B, nm: => B)(f: A => B): B = this match {
     case NA => na
     case NM => nm
     case Value(a) => f(a)
   }
 
-  def getOrElse[B >: A](default: => B): B = this match {
-    case Value(a) => a
-    case _ => default
+  /** Returns the [[Cell]]'s value if it is available and meaningful,
+    * otherwise returns the result of evaluating expression `default`.
+    *
+    * @param  default  the default expression.
+    * @see [[get]]
+    * @see [[orElse]]
+    */
+  @inline final def getOrElse[B >: A](default: => B): B =
+    if (isValue) this.get else default
+
+  /** Returns a [[Value]] containing the result of applying `f` to
+    * this [[Cell]]'s value if it is available and meaningful.
+    *
+    * @param  f  the function to apply to a value that is available and meaningful.
+    * @see [[flatMap]]
+    * @see [[foreach]]
+    */
+  @inline final def map[B](f: A => B): Cell[B] =
+    if (isValue) Value(f(this.get)) else this.asInstanceOf[Cell[B]]
+
+  /** Returns the result of applying `f` to this [[Cell]]'s value if
+    * it is available and meaningful.
+    *
+    * @param  f  the function to apply to a value that is available and meaningful.
+    * @see [[map]]
+    * @see [[foreach]]
+    */
+  @inline final def flatMap[B](f: A => Cell[B]): Cell[B] =
+    if (isValue) f(this.get) else this.asInstanceOf[Cell[B]]
+
+  /** Flatten a nested [[Cell]] with type `Cell[Cell[B]]` into `Cell[B]`.
+    *
+    * @note there must be implicit evident that `A` is a subtype of `Cell[B]`.
+    */
+  @inline final def flatten[B](implicit ev: A <:< Cell[B]): Cell[B] =
+    if (isValue) ev(this.get) else this.asInstanceOf[Cell[B]]
+
+  /** Returns this [[Cell]] unless it contains a value that is
+    * available and meaningful ''and'' applying the predicate `p` to
+    * that value returns false, then return [[NA]].
+    *
+    * @param  p  the predicate used to test a value that is available and meaningful.
+    * @see [[filterNot]]
+    * @see [[collect]]
+    */
+  @inline final def filter(p: A => Boolean): Cell[A] =
+    if (isNonValue || p(this.get)) this else NA
+
+  /** Returns this [[Cell]] unless it contains a value that is
+    * available and meaningful ''and'' applying the predicate `p` to
+    * that value returns true, then return [[NA]].
+    *
+    * @param  p  the predicate to test a value that is available and meaningful.
+    * @see [[filter]]
+    * @see [[collect]]
+    */
+  @inline final def filterNot(p: A => Boolean): Cell[A] =
+    if (isNonValue || !p(this.get)) this else NA
+
+  /** Returns true if this [[Cell]]'s value is available and
+    * meaningful ''and'' the predicate `p` returns true when applied
+    * to that value. Otherwise, returns false.
+    *
+    * @param  p  the predicate to test a value that is available and meaningful.
+    * @see [[forall]]
+    */
+  @inline final def exists(p: A => Boolean): Boolean =
+    isValue && p(this.get)
+
+  /** Returns true if this [[Cell]]'s value is unavailable ([[NA]])
+    *''or'' the predicate `p` returns true when applied to this
+    * [[Cell]]'s meaningful value.
+    *
+    * @note [[NA]] represents the vacuous case, so will result in
+    * true, but [[NM]] will result in false.
+    *
+    * @param  p  the predicate to test a value that is available and meaningful.
+    * @see [[exists]]
+    */
+  @inline final def forall(p: A => Boolean): Boolean = this match {
+    case NA => true
+    case NM => false
+    case Value(a) => p(a)
   }
 
-  def map[B](f: A => B): Cell[B] = this match {
-    case Value(a) => Value(f(a))
-    case NA => NA
+  /** Apply the the given procedure `f` to the [[Cell]]'s value if it
+    * is available and meaningful. Otherwise, do nothing.
+    *
+    * @param  f  the procedure to apply to a value that is available and meaningful.
+    * @see [[map]]
+    * @see [[flatMap]]
+    */
+  @inline final def foreach[U](f: A => U): Unit =
+    if (isValue) f(this.get)
+
+  /** Returns a [[Value]] containing the result of appling `pf` to
+    * this [[Cell]]'s value if it is available and meaningful ''and''
+    * `pf` is defined for that value. Otherwise return [[NA]], unless
+    * this [[Cell]] is [[NM]].
+    *
+    * @param  pf  the partial function to apply to a value that is available and meaningful.
+    * @see [[filter]]
+    * @see [[filterNot]]
+    */
+  @inline final def collect[B](pf: PartialFunction[A, B]): Cell[B] = this match {
     case NM => NM
+    case Value(a) if pf.isDefinedAt(a) => Value(pf(a))
+    case _ => NA
   }
 
-  def flatMap[B](f: A => Cell[B]): Cell[B] = this match {
-    case Value(a) => f(a)
-    case NA => NA
-    case NM => NM
-  }
+  /** Returns this [[Cell]] if its value is available and meaningful,
+    * otherwise return the result of evaluating `alternative`.
+    *
+    * @param  alternative  the alternative expression
+    * @see [[getOrElse]]
+    */
+  @inline final def orElse[B >: A](alternative: => Cell[B]): Cell[B] =
+    if (isValue) this else alternative
 
-  def filter(f: A => Boolean): Cell[A] = this match {
-    case Value(a) if !f(a) => NA
-    case other => other
-  }
+  /** Project this [[Cell]] to an `Option`
+    *
+    * @return Some of the value or None for non values
+    */
+  @inline final def toOption: Option[A] = value
 
-  def foreach[U](f: A => U): Unit = this match {
-    case Value(a) => f(a)
-    case _ =>
-  }
+  /** Returns a singleton list containing the [[Cell]]'s value, or
+    * the empty list if the [[Cell]]'s value is unavailable or not
+    * meaningful.
+    */
+  @inline final def toList: List[A] =
+    if (isValue) List(this.get) else List.empty
 }
 
 // TODO: there are currently issues where we get comparison between Value(NA) and NA and this should be true
@@ -70,7 +225,7 @@ object Cell extends CellInstances {
     case None => nonValue
   }
 
-  implicit def cell2Iterable[A](cell: Cell[A]): Iterable[A] = cell.value.toList
+  implicit def cell2Iterable[A](cell: Cell[A]): Iterable[A] = cell.toList
 }
 
 /** The supertype of non values, [[NA]] (''Not Available'') and
@@ -95,7 +250,10 @@ sealed trait NonValue extends Cell[Nothing] {
   *
   * @see [[Cell]] [[NonValue]] [[NM]] [[Value]]
   */
-case object NA extends NonValue { val valueString = "NA" }
+case object NA extends NonValue {
+  def get = throw new NoSuchElementException("NA.get")
+  val valueString = "NA"
+}
 
 
 /** The value is ''Not Meaningful (NM)''.
@@ -107,7 +265,10 @@ case object NA extends NonValue { val valueString = "NA" }
   *
   * @see [[Cell]] [[NonValue]] [[NA]] [[Value]]
   */
-case object NM extends NonValue { val valueString = "NM" }
+case object NM extends NonValue {
+  def get = throw new NoSuchElementException("NM.get")
+  val valueString = "NM"
+}
 
 
 /** A value that is meaningful.
