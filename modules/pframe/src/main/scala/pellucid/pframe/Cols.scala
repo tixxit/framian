2f085f5ad30fbf9f02391d1b7d076bb9d540e7af
Tom Switzer <thomas.switzer@gmail.com>
2014-05-30 15:49:38 -0400

    Generalize Cols code to be re-used in Rows.

--- modules/pframe/src/main/scala/pellucid/pframe/Cols.scala
+++ modules/pframe/src/main/scala/pellucid/pframe/Cols.scala
@@ -1,5 +1,7 @@
 package pellucid.pframe
 
+import scala.language.higherKinds
+
 import scala.annotation.implicitNotFound
 import scala.collection.SortedMap
 import scala.reflect.ClassTag
@@ -18,66 +20,36 @@ sealed trait Cols[K, A] {
   def map[B](f: A => B): Cols[K, B]
 }
 
-object Cols extends ColsFunctions {
-  sealed trait SizedCols[K, Sz <: Size, A] extends Cols[K, A] {
-    val extractor: RowExtractor[A, K, Sz]
+sealed trait SizedCols[K, Sz <: Size, A] extends Cols[K, A] {
+  val extractor: RowExtractor[A, K, Sz]
 
-    def fold[B](all: => B)(f: List[K] => B): B
-    def as[B](implicit extractor0: RowExtractor[B, K, Sz]): SizedCols[K, Sz, B]
-  }
-
-  final class AllCols[K, A](val extractor: RowExtractor[A, K, Variable])
-      extends SizedCols[K, Variable, A] {
-    def fold[B](all: => B)(f: List[K] => B): B = all
-    def as[B](implicit extractor0: RowExtractor[B, K, Variable]): AllCols[K, B] =
-      new AllCols[K, B](extractor0)
-    def map[B](f: A => B): AllCols[K, B] =
-      new AllCols[K, B](extractor map f)
-  }
-
-  final class SomeCols[K, S <: Size, A](val keys: List[K], val extractor: RowExtractor[A, K, S])
-      extends SizedCols[K, S, A] {
-    def fold[B](all: => B)(f: List[K] => B): B = f(keys)
-    def as[B](implicit extractor0: RowExtractor[B, K, S]): SomeCols[K, S, B] =
-      new SomeCols[K, S, B](keys, extractor0)
-    def map[B](f: A => B): SomeCols[K, S, B] =
-      new SomeCols[K, S, B](keys, extractor map f)
-  }
+  def fold[B](all: => B)(f: List[K] => B): B
+  def as[B](implicit extractor0: RowExtractor[B, K, Sz]): SizedCols[K, Sz, B]
 }
 
-trait ColsFunctions {
-  import Cols._
-  import Nat._
+final class AllCols[K, A](val extractor: RowExtractor[A, K, Variable])
+    extends SizedCols[K, Variable, A] {
+  def fold[B](all: => B)(f: List[K] => B): B = all
+  def as[B](implicit extractor0: RowExtractor[B, K, Variable]): AllCols[K, B] =
+    new AllCols[K, B](extractor0)
+  def map[B](f: A => B): AllCols[K, B] =
+    new AllCols[K, B](extractor map f)
+}
 
+final class SomeCols[K, S <: Size, A](val keys: List[K], val extractor: RowExtractor[A, K, S])
+    extends SizedCols[K, S, A] {
+  def fold[B](all: => B)(f: List[K] => B): B = f(keys)
+  def as[B](implicit extractor0: RowExtractor[B, K, S]): SomeCols[K, S, B] =
+    new SomeCols[K, S, B](keys, extractor0)
+  def map[B](f: A => B): SomeCols[K, S, B] =
+    new SomeCols[K, S, B](keys, extractor map f)
+}
+
+object Cols extends ColsFunctions
+
+trait ColsFunctions extends SizedConstructor[SomeCols] {
   def all[K] = new AllCols[K, Rec[K]](RowExtractor[Rec[K], K, Variable])
 
   def sized[K, N <: Nat](s: Sized[List[K], N]): SomeCols[K, Fixed[N], Rec[K]] =
     new SomeCols[K, Fixed[N], Rec[K]](s.unsized, RowExtractor[Rec[K], K, Fixed[N]])
-
-  def apply[K](c0: K): SomeCols[K, Fixed[_1], Rec[K]] =
-    sized(Sized[List](c0))
-
-  def apply[K](c0: K, c1: K): SomeCols[K, Fixed[_2], Rec[K]] =
-    sized(Sized[List](c0, c1))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K): SomeCols[K, Fixed[_4], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K): SomeCols[K, Fixed[_5], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K, c5: K): SomeCols[K, Fixed[_6], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4, c5))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K, c5: K, c6: K): SomeCols[K, Fixed[_7], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4, c5, c6))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K, c5: K, c6: K, c7: K): SomeCols[K, Fixed[_8], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4, c5, c6, c7))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K, c5: K, c6: K, c7: K, c8: K): SomeCols[K, Fixed[_9], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4, c5, c6, c7, c8))
-
-  def apply[K](c0: K, c1: K, c2: K, c3: K, c4: K, c5: K, c6: K, c7: K, c8: K, c9: K): SomeCols[K, Fixed[_10], Rec[K]] =
-    sized(Sized[List](c0, c1, c2, c3, c4, c5, c6, c7, c8, c9))
 }
---
+++ modules/pframe/src/main/scala/pellucid/pframe/Rows.scala
@@ -0,0 +1,12 @@
+package pellucid.pframe
+
+sealed trait Rows[K, A] extends AxisSelectionLike[K, A, Rows]
+object Rows extends AxisSelectionCompanion {
+  type AxisSelection[K, A] = Rows[K, A]
+
+  case class All[K, A](extractor: RowExtractor[A, K, Variable]) extends Rows[K, A] with AllAxisSelection[K, A]
+  object All extends AllCompanion
+
+  case class Pick[K, S <: Size, A](keys: List[K], extractor: RowExtractor[A, K, S]) extends Rows[K, A] with PickAxisSelection[K, S, A]
+  object Pick extends PickCompanion
+}
