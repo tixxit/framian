b49626e0aa5cd667ac2f96506d6d98929ea0ff25
Daniel James <dwhjames@gmail.com>
2014-08-06 13:56:23 -0400

    Build.scala must die
    - factor out build into main build.sbt and sub project build.sbt
    - bump 2.11.1 to 2.11.2
    - factor out common settings for publishing and test coverage
    - add settings for framian scaladoc
    - use provided scope for Play dependencies

---
+++ project/Dependencies.scala
@@ -0,0 +1,42 @@
+import sbt._
+
+object Dependencies {
+
+  object V {
+    val Play_22            = "2.2.3"
+    val Play_23            = "2.3.1"
+
+    val Spire              = "0.7.4"
+    val Shapeless          = "2.0.0"
+    val Discipline         = "0.2.1"
+
+    val JodaTime           = "2.3"
+    val JodaConvert        = "1.6"
+
+    // Test libraries
+    val Specs2             = "2.3.12"
+    val ScalaCheck         = "1.11.4"
+  }
+
+  // Compile
+  object Compile {
+    val playJson_22       = "com.typesafe.play"        %% "play-json"               % V.Play_22 % "provided"
+    val playJson_23       = "com.typesafe.play"        %% "play-json"               % V.Play_23 % "provided"
+
+    val spire             = "org.spire-math"           %% "spire"                   % V.Spire
+    val shapeless_10      = "com.chuusai"               % "shapeless_2.10.4"        % V.Shapeless
+    val shapeless_11      = "com.chuusai"               % "shapeless_2.11"          % V.Shapeless
+
+    val jodaTime          = "joda-time"                 % "joda-time"               % V.JodaTime
+    val jodaConvert       = "org.joda"                  % "joda-convert"            % V.JodaConvert
+  }
+
+  // Test
+  object Test {
+    val specs2          =   "org.specs2"            %% "specs2"                      % V.Specs2        % "test"
+    val scalaCheck      =   "org.scalacheck"        %% "scalacheck"                  % V.ScalaCheck    % "test"
+    val spireLaws       =   "org.spire-math"        %% "spire-scalacheck-binding"    % V.Spire         % "test"
+    val discipline      =   "org.typelevel"         %% "discipline"                  % V.Discipline    % "test"
+  }
+}
+
