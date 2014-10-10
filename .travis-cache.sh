1937a20113ffbef46400d6d31a70020e265691a9
Daniel James <dwhjames@gmail.com>
2014-10-10 11:31:28 -0400

    use sbt-extras -no-share option
    Use entirely local cache, for both ivy and sbt’s boot. The cache should relieve the need to build the compiler interface on demand.

--- .travis-cache.sh
+++ .travis-cache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L http://s3.amazonaws.com/pellucidanalytics-framian-ivy2-cache/ivy2-cache.tar.gz | tar xzf - -C $HOME
+curl -L https://s3.amazonaws.com/pellucidanalytics-travis-ci-cache/framian.tar.gz | tar xzf -
 
 # always succeed: the cache is optional
 exit 0
